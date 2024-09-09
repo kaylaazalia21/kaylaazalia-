d24fa8906c8e026f3ba7625ba342a0ddbd0ddec8
Kayla Azalia <kaylaazalia38@gmail.com>
2024-09-09 11:19:45 +0700

    Add files via upload

---
+++ pertemuan kedua.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//deklarasi variabel
+	var nama string = "Kayla Azalia"
+	var umur int = 18
+	var alamat string = "Jalan Kramat I Nomor 1"
+	var berkacamata bool = false
+	var minuskiri float32 = 0.50
+	var minuskanan float32 = 0.50
+
+	//proses
+	var hasilminus float32 = minuskiri + minuskanan
+
+	//output
+	fmt.Println(nama)
+	fmt.Println(umur)
+	fmt.Println(alamat)
+	fmt.Println(berkacamata)
+	fmt.Println(hasilminus)
+
+}
